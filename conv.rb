7c2536d650d844f0070eb2ab3a1974993960a406
Otec Fura <fura.otec@gmail.com>
2014-11-25 01:34:29 +0100

    bug fix

--- conv.rb
+++ conv.rb
@@ -4,14 +4,14 @@ require "cgi"
 
 arg = ARGV[0]
 file =  File.open(ARGV[1])
+$language = ARGV[2]
+
+if $language == nil
+  puts "Specify language!"
+  exit
+end
 
 def csvToXML(fileXML)
-    language = ARGV[2]
-    
-    if language == nil
-      puts "Specify language!"
-      exit
-    end
 
     results = File.read(fileXML)
     csv_table = CSV.parse(results, :headers => true)
@@ -19,7 +19,7 @@ def csvToXML(fileXML)
     builder = Nokogiri::XML::Builder.new do |xml|
       xml.resources{
          csv_table.entries.each do |csv_row|
-          	xml.string "string", csv_row[language], :name => csv_row["name"] 
+          	xml.string "string", csv_row[$language], :name => csv_row["name"] 
           end
       }
     end
@@ -30,6 +30,7 @@ if arg == "xml"
   str = ""
   xml_doc = Nokogiri::XML(file)
   array = xml_doc.xpath("//string")
+  str.concat("\"name\", \"#{$language}\"\n")
 	array.each do |xpath_node|
   	str.concat( "\"#{xpath_node.attribute('name')}\", \"#{xpath_node.text}\"\n")
   end
